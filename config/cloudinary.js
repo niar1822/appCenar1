7c3194b9990aedc4e62b8587a7a36cb47e074e33
Nasser Collado <nasserisraelcollado@gmail.com>
2026-07-28 05:43:38 +0200

    cambios a la nuve

---
+++ config/cloudinary.js
@@ -0,0 +1,14 @@
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+console.log({
+  cloud: process.env.CLOUDINARY_CLOUD_NAME,
+  key: process.env.CLOUDINARY_API_KEY,
+});
+
+export default cloudinary;
